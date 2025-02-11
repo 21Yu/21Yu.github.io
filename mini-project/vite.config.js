b4dfa4315f8ca79e738af7cf80ce27a6a889eb53
21Yu <12yudev@gmail.com>
2025-02-11 15:56:51 -0800

    update base in vite.config, and homepage link in package.json

--- mini-project/vite.config.js
+++ mini-project/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/21Yu.github.io/',  // Change this to match your GitHub repo name
+  base: './',  // Use '/' for GitHub user pages
 })
