824fdf3bb3caf8829f3adf94ba0ae3bc66d733b2
21Yu <12yudev@gmail.com>
2025-02-11 19:20:16 -0800

    create navbar component  with material UI

--- mini-project/src/components/Navbar.jsx
+++ mini-project/src/components/Navbar.jsx
@@ -1,15 +1,28 @@
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { AppBar, Tabs, Tab, Toolbar, Typography, Box } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
 
-function Navbar() {
+const Navbar = () => {
+  const location = useLocation();
+  const [value, setValue] = useState(location.pathname);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-      </ul>
-    </nav>
+    <AppBar position="static">
+      <Toolbar>
+        <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
+          <Tabs value={value} onChange={handleChange} textColor="inherit" indicatorColor="secondary">
+            <Tab label="Home" value="/" component={Link} to="/" />
+            <Tab label="About" value="/about" component={Link} to="/about" />
+            <Tab label="Projects" value="/projects" component={Link} to="/projects" />
+          </Tabs>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
 export default Navbar;
