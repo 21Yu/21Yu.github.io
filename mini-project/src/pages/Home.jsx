5c3945cf5fef3d6201a962df9d28676dd072e809
21Yu <12yudev@gmail.com>
2025-02-12 01:36:26 -0800

    added social media links section on home page

--- mini-project/src/pages/Home.jsx
+++ mini-project/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Typography, Card, CardContent } from '@mui/material';
+import { Box, Typography, Card, CardContent, Link} from '@mui/material';
 import Skills from '../components/skills';  // Import the Skills component
+import { FaGithub, FaLinkedin, FaTwitter, FaFacebook } from "react-icons/fa"; // Add social media icons
 
 function Home() {
     return (
@@ -27,7 +28,25 @@ function Home() {
             </Card>
 
             {/* Right-hand side card containing the Skills component */}
-            <Skills />
+            <Box sx={{ flex: 1 }}>
+                <Skills />
+
+                {/* Social Media Links (Placed below Skills) */}
+                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 3, mt: 8 }}>
+                    <Link href="https://github.com/21Yu" target="_blank" sx={{ color: 'inherit' }}>
+                        <FaGithub size={30} />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/yu-wu-a283492a5/" target="_blank" sx={{ color: 'inherit' }}>
+                        <FaLinkedin size={30} />
+                    </Link>
+                    {/* <Link href="https://twitter.com/yourusername" target="_blank" sx={{ color: 'inherit' }}>
+                        <FaTwitter size={30} />
+                    </Link>
+                    <Link href="https://www.facebook.com/yourusername" target="_blank" sx={{ color: 'inherit' }}>
+                        <FaFacebook size={30} />
+                    </Link> */}
+                </Box>
+            </Box>
         </Box>
     );
 }
